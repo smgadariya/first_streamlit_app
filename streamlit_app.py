366a45599f073a3c3649a98c33a66bab7d0cc086
Saurabh Gadariya <gadariyasaurabh@gmail.com>
2022-06-17 18:30:50 +0100

    Update streamlit_app.py
    First commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
+import streamlit
+
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
 
