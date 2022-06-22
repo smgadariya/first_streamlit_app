ba114f04edff2eb1e413c8d5803de70d5e8a8eb8
Saurabh Gadariya <gadariyasaurabh@gmail.com>
2022-06-22 22:41:30 +0100

    use of request

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.text('Omega 3 and Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-import pandas
+import pandashttps://github.com/smgadariya/first_streamlit_app/blob/main/streamlit_app.py
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -16,4 +16,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
