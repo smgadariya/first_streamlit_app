98d0aa37a2bfd54cc6451737e01320f0b28a778d
Saurabh Gadariya <gadariyasaurabh@gmail.com>
2022-06-22 23:21:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,5 @@ streamlit.header('Fruityvice fruit advice!')
 import requests
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response).json()
+streamlit.text(fruityvice_response.json())
 
