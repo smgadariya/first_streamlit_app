0a4fb62cdc59f6d1fba8ac2f27988ee3ba65126a
Saurabh Gadariya <gadariyasaurabh@gmail.com>
2022-06-25 13:04:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ my_cur = my_cnx.cursor()
 #streamlit.text("Hello from Snowflake:")
 #streamlit.text(my_data_row)
 
-my_cur.execute("select * from fruit_load_list")
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows=my_cur.fetchall()
 streamlit.dataframe(my_data_rows)
 
